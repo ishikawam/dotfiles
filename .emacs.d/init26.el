e0e0a554344eedda64ed90efcbd9164cd4d99de8
M_Ishikawa <ishikawam@nifty.com>
2018-11-02 11:18:13 +0900

    private update and emacs26 conf

--- .emacs.d/init26.el
+++ .emacs.d/init26.el
@@ -57,13 +57,76 @@
 
 ;(use-package helm-config) ; できない
 
+;;; GNU GLOBAL for helm
 (use-package helm-gtags)
+(add-hook 'go-mode-hook (lambda () (helm-gtags-mode)))
+(add-hook 'python-mode-hook (lambda () (helm-gtags-mode)))
+(add-hook 'ruby-mode-hook (lambda () (helm-gtags-mode)))
+(setq helm-gtags-path-style 'root)
+(setq helm-gtags-auto-update t)
+(add-hook 'helm-gtags-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
+             (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
+             (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+             (local-set-key (kbd "M-e") 'helm-gtags-pop-stack)
+;             (local-set-key (kbd "M-g") 'helm-gtags-dwim)
+;             (local-set-key (kbd "M-s") 'helm-gtags-show-stack)
+;             (local-set-key (kbd "M-p") 'helm-gtags-previous-history)
+;             (local-set-key (kbd "M-n") 'helm-gtags-next-history)
+             ))
+
+(autoload 'gtags-mode "gtags" "" t)
+(setq gtags-mode-hook
+      '(lambda ()
+         (local-set-key "\M-t" 'gtags-find-tag)
+         (local-set-key "\M-r" 'gtags-find-rtag)
+         (local-set-key "\M-s" 'gtags-find-symbol)
+         (local-set-key "\M-e" 'gtags-pop-stack)
+         ))
+(global-set-key "\M-e" 'gtags-pop-stack)
+(add-hook 'c-mode-common-hook
+          '(lambda()
+             (helm-gtags-mode 1)
+             ))
 
 (use-package auto-install)
 
+
 (use-package monokai-theme)
+(custom-theme-set-faces
+ 'monokai
+ ;; メニューバー = header-line
+ '(menu ((t (:foreground "#111111" :background "#CCCCCC")))))
+;  :init
+;  (custom-theme-set-faces
+;   'monokai
+;   ;; メニューバー = header-line
+;   '(menu ((t (:foreground "#111111" :background "#CCCCCC")))))
+;  )
 
 (use-package php-mode)
+;;; php hook
+(add-hook 'php-mode-hook
+          (lambda ()
+            (c-set-style "stroustrup")
+            (setq tab-width 4)
+            (setq c-basic-offset 4)
+;            (setq indent-tabs-mode t)    ; インデント：タブ文字
+;            (setq indent-tabs-mode nil)    ; インデント：空白スペース
+            (c-set-offset 'case-label' 4)
+            (c-set-offset 'arglist-intro' 4)
+            (c-set-offset 'arglist-cont-nonempty' 4)
+            (c-set-offset 'arglist-close' 0)
+            (electric-indent-local-mode -1)
+            (c-toggle-electric-state -1)
+            ))
+
+
+;;; php5
+(setq auto-mode-alist
+      (append '(("\\.\\(php5\\)$" . php-mode))
+              auto-mode-alist))
 
 (use-package geben)
 
