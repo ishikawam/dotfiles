df5c537945234658042bdb513b9691dfc955b913
M_Ishikawa <ishikawam@nifty.com>
2019-01-09 11:40:44 +0900

    set hostname for mac

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -31,42 +31,68 @@ if [ ! $loginshell = 'zsh' ]; then
     else
         chsh -s `which zsh`
     fi
+
+    echo $loginshell -> `which zsh`
+else
+    echo Do nothing.
 fi
 
 
-# スクリプト等インストール yum apt homebrew
-# ToDo
+######## mac homebrew ##################################################################
 
-
-# mac homebrew
-head "2. Homebrew"
+head "2. Homebrew (mac)"
 if [ `uname` = "Darwin" ]; then
     if [ ! -x "`which brew 2>/dev/null`" ]; then
         # http://brew.sh/index_ja.html
         echo Install Homebrew.
         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
         brew doctor
-# チャレンジしたい
-#        brew cask install firefox vagrant virtualbox mysqlworkbench skitch evernote google-japanese-ime
     fi
     brew update
     brew upgrade
     brew install tmux gnu-sed mysql tig wget emacs git colordiff global peco imagemagick telnet jq npm
     brew cask install docker sublime-text macdown alfred dropbox karabiner-elements google-chrome
+    brew cask install firefox mysqlworkbench skitch evernote google-japanese-ime
+# チャレンジしたい
+#        brew cask install firefox vagrant virtualbox mysqlworkbench skitch evernote google-japanese-ime
     # 途中karabiner-elementsでFumihiko Takayamaを許可するかどうか出てくるので許可を。
-    # 初回は全部開いてダイアログでagreeを @todo; agreeないのだけにしたい
+    # 全部開いてダイアログでagreeを @todo; agreeないのだけにしたい
     open /Applications/Docker.app
-    open /Applications/Sublime\ Text.app
-    open /Applications/MacDown.app
+#    open /Applications/Sublime\ Text.app
+#    open /Applications/MacDown.app
     open /Applications/Alfred\ 3.app
     open /Applications/Dropbox.app
     open /Applications/Karabiner-Elements.app
-    open /Applications/Google\ Chrome.app
+#    open /Applications/Google\ Chrome.app
+#    open /Applications/Firefox.app
+#    open /Applications/MySQLWorkbench.app
+#    open /Applications/Skitch.app
+    open /Applications/Evernote.app
+else
+    echo Do nothing.
 fi
 
 
+######## mac homebrew ##################################################################
+
+head "3. hostname (mac)"
+if [ `uname` = "Darwin" ]; then
+    HOSTNAME=`scutil --get ComputerName`
+    echo ComputerName: $HOSTNAME
+    echo HostName: `scutil --get HostName` -> $HOSTNAME
+    sudo scutil --set HostName "$HOSTNAME"
+    echo LocalHostName: `scutil --get LocalHostName` -> $HOSTNAME
+    sudo scutil --set LocalHostName "$HOSTNAME"
+else
+    echo Do nothing.
+fi
+
+
+######## symbolic links ##################################################################
+
+head "4. symbolic links"
+
 # mlocate for mac
-head "3. symbolic links"
 if [ -f /usr/libexec/locate.updatedb -a ! -f ~/this/bin/updatedb ]; then
     # エイリアスにしていないのは、sudoで使いたいから
     ln -s /usr/libexec/locate.updatedb ~/this/bin/updatedb
@@ -89,21 +115,21 @@ fi
 if [ -x "`which cask 2>/dev/null`" ]; then
     # python2.6以上に依存＞cask
     # pyenv install 2.7.9
-    head "4. cask install"
+    head "5. cask install"
     cd ~/.emacs.d/ ; cask install ; cd -
 fi
 
 
 ######## ssh config ##################################################################
 
-head "5. ssh config"
+head "6. ssh config"
 mkdir -p -m 700 ~/.ssh
 chmod 600 ~/.ssh/config
 
 
 ######## private ##################################################################
 
-head "6. private setup"
+head "7. private setup"
 if [ -f ~/private/scripts/setup_private.sh ]; then
     sh ~/private/scripts/setup_private.sh
 fi
@@ -111,6 +137,7 @@ fi
 
 ######## done ##################################################################
 
+head "Finished."
 echo
 echo "please 'source ~/.zshrc'"
 echo
