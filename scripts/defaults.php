3a3b1e5d4605a83b8eceab5e0728936d5b28e51b
M_Ishikawa <ishikawam@nifty.com>
2019-01-10 16:33:10 +0900

    defaults.php

--- scripts/defaults.php
+++ scripts/defaults.php
@@ -4,6 +4,8 @@
 
 ### https://qiita.com/djmonta/items/17531dde1e82d9786816
 
+$HOME = exec('echo $HOME');
+
 $arr = [
     'com.apple.dock' => [
         'autohide' => [
@@ -94,67 +96,62 @@ $arr = [
             'read' => 1,
             'write' => '-bool true',
         ],
+        'NewWindowTarget' => [
+            // 新しいウィンドウでデフォルトでホームフォルダを開く
+            'read' => 'PfHm',
+            'write' => '-string PfHm',
+        ],
+        'NewWindowTargetPath' => [
+            // 新しいウィンドウでデフォルトでホームフォルダを開く
+            'read' => 'file://' . $HOME . '/',
+            'write' => '-string file://' . $HOME . '/',
+        ],
+        'ShowStatusBar' => [
+            // Show Status bar in Finder （ステータスバーを表示）
+            'read' => 1,
+            'write' => '-bool true',
+        ],
+        'ShowPathbar' => [
+            // Show Path bar in Finder （パスバーを表示）
+            'read' => 1,
+            'write' => '-bool true',
+        ],
+        'ShowTabView' => [
+            // Show Tab bar in Finder （タブバーを非表示）
+            'read' => 0,
+            'write' => '-bool false',
+        ],
+        'ShowExternalHardDrivesOnDesktop' => [
+            // Show icons for hard drives, servers, and removable media on the desktop
+            'read' => 1,
+            'write' => '-bool true',
+        ],
+        'ShowHardDrivesOnDesktop' => [
+            // Show icons for hard drives, servers, and removable media on the desktop
+            'read' => 1,
+            'write' => '-bool true',
+        ],
+        'ShowMountedServersOnDesktop' => [
+            // Show icons for hard drives, servers, and removable media on the desktop
+            'read' => 1,
+            'write' => '-bool true',
+        ],
+        'ShowRemovableMediaOnDesktop' => [
+            // Show icons for hard drives, servers, and removable media on the desktop
+            'read' => 1,
+            'write' => '-bool true',
+        ],
+        'ShowRemovableMediaOnDesktop' => [
+            // Show icons for hard drives, servers, and removable media on the desktop
+            'read' => 1,
+            'write' => '-bool true',
+        ],
 /*
-# Allow you to select and copy string in QuickLook （QuickLook で文字の選択、コピーを出来るようにする）
-defaults write com.apple.finder QLEnableTextSelection -bool true
-
-# Set `${HOME}` as the default location for new Finder windows
-# 新しいウィンドウでデフォルトでホームフォルダを開く
-defaults write com.apple.finder NewWindowTarget -string "PfDe"
-defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
-
-# Show Status bar in Finder （ステータスバーを表示）
-defaults write com.apple.finder ShowStatusBar -bool true
-
-# Show Path bar in Finder （パスバーを表示）
-defaults write com.apple.finder ShowPathbar -bool true
-
-# Show Tab bar in Finder （タブバーを表示）
-defaults write com.apple.finder ShowTabView -bool true
 
 
-# Show the hidden files （不可視ファイルを表示）
-defaults write com.apple.finder AppleShowAllFiles YES
 
 
-###############################################################################
-# Finder                                                                      #
-###############################################################################
 
-# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
-defaults write com.apple.finder QuitMenuItem -bool true
-
-# Finder: disable window animations and Get Info animations
-defaults write com.apple.finder DisableAllAnimations -bool true
-
-# Set Desktop as the default location for new Finder windows
-# For other paths, use `PfLo` and `file:///full/path/here/`
-defaults write com.apple.finder NewWindowTarget -string "PfDe"
-defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
-
-# Show icons for hard drives, servers, and removable media on the desktop
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-
-# Finder: show hidden files by default
-#defaults write com.apple.finder AppleShowAllFiles -bool true
-
-# Finder: show all filename extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-# Finder: show status bar
-defaults write com.apple.finder ShowStatusBar -bool true
-
-# Finder: show path bar
-defaults write com.apple.finder ShowPathbar -bool true
-
-# Display full POSIX path as Finder window title
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-
-# Keep folders on top when sorting by name
-defaults write com.apple.finder _FXSortFoldersFirst -bool true
 
 # When performing a search, search the current folder by default
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
@@ -162,11 +159,6 @@ defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 # Disable the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# Enable spring loading for directories
-defaults write NSGlobalDomain com.apple.springing.enabled -bool true
-
-# Remove the spring loading delay for directories
-defaults write NSGlobalDomain com.apple.springing.delay -float 0
 
 # Avoid creating .DS_Store files on network or USB volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
@@ -178,8 +170,6 @@ defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
 # Automatically open a new Finder window when a volume is mounted
-defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
-defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
 defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
 # Show item info near icons on the desktop and in other icon views
@@ -250,7 +240,11 @@ foreach ($arr as $com => $tmp) {
         }
         if ($read == '') {
             echo "\033[31mError. $com $attr\033[0m\n";
-            continue;
+            echo 'regist? y or n (n): ';
+            $input = trim(fgets(STDIN));
+            if ($input != 'y') {
+                continue;
+            }
         }
 
         if (! isset($val['write']) || $read == $val['read']) {
@@ -270,17 +264,6 @@ foreach ($arr as $com => $tmp) {
 }
 
 /*
-
-
-
-# Automatically open a new Finder window when a volume is mounted
-# マウントされたディスクがあったら、自動的に新しいウィンドウを開く
-defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
-defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
-
-
-
-
 
 # Enable `Tap to click` （タップでクリックを有効にする）
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
@@ -1150,6 +1133,16 @@ defaults write com.apple.helpviewer DevMode -bool true
 #echo "0x08000100:0" > ~/.CFUserTextEncoding
 ### (略)
 
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+
+# Enable spring loading for directories
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+
+# Remove the spring loading delay for directories
+defaults write NSGlobalDomain com.apple.springing.delay -float 0
 
 */
 
