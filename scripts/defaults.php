cb6169a4c4f5bdc82c82b95f9a886f0a7fb6f08f
M_Ishikawa <ishikawam@nifty.com>
2019-01-13 15:27:06 +0900

    defaults.php

--- scripts/defaults.php
+++ scripts/defaults.php
@@ -622,6 +622,123 @@ defaults write com.apple.finder FXInfoPanesExpanded -dict \
             'write' => '-bool true',
         ],
     ],
+
+    // Spotlight
+    'com.apple.Spotlight' => [
+        'orderedItems' => [
+            'read' => '(
+        {
+        enabled = 0;
+        name = APPLICATIONS;
+    },
+        {
+        enabled = 0;
+        name = "MENU_SPOTLIGHT_SUGGESTIONS";
+    },
+        {
+        enabled = 0;
+        name = "MENU_CONVERSION";
+    },
+        {
+        enabled = 0;
+        name = "MENU_EXPRESSION";
+    },
+        {
+        enabled = 0;
+        name = "MENU_DEFINITION";
+    },
+        {
+        enabled = 0;
+        name = "SYSTEM_PREFS";
+    },
+        {
+        enabled = 0;
+        name = DOCUMENTS;
+    },
+        {
+        enabled = 0;
+        name = DIRECTORIES;
+    },
+        {
+        enabled = 0;
+        name = PRESENTATIONS;
+    },
+        {
+        enabled = 0;
+        name = SPREADSHEETS;
+    },
+        {
+        enabled = 0;
+        name = PDF;
+    },
+        {
+        enabled = 0;
+        name = MESSAGES;
+    },
+        {
+        enabled = 0;
+        name = CONTACT;
+    },
+        {
+        enabled = 1;
+        name = "EVENT_TODO";
+    },
+        {
+        enabled = 0;
+        name = IMAGES;
+    },
+        {
+        enabled = 0;
+        name = BOOKMARKS;
+    },
+        {
+        enabled = 0;
+        name = MUSIC;
+    },
+        {
+        enabled = 0;
+        name = MOVIES;
+    },
+        {
+        enabled = 0;
+        name = FONTS;
+    },
+        {
+        enabled = 0;
+        name = "MENU_OTHER";
+    },
+        {
+        enabled = 0;
+        name = SOURCE;
+    }
+)',
+            'write' => '-array \
+	\'{"enabled" = 0;"name" = "APPLICATIONS";}\' \
+	\'{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}\' \
+	\'{"enabled" = 0;"name" = "MENU_CONVERSION";}\' \
+	\'{"enabled" = 0;"name" = "MENU_EXPRESSION";}\' \
+	\'{"enabled" = 0;"name" = "MENU_DEFINITION";}\' \
+	\'{"enabled" = 0;"name" = "SYSTEM_PREFS";}\' \
+	\'{"enabled" = 0;"name" = "DOCUMENTS";}\' \
+	\'{"enabled" = 0;"name" = "DIRECTORIES";}\' \
+	\'{"enabled" = 0;"name" = "PRESENTATIONS";}\' \
+	\'{"enabled" = 0;"name" = "SPREADSHEETS";}\' \
+	\'{"enabled" = 0;"name" = "PDF";}\' \
+	\'{"enabled" = 0;"name" = "MESSAGES";}\' \
+	\'{"enabled" = 0;"name" = "CONTACT";}\' \
+	\'{"enabled" = 1;"name" = "EVENT_TODO";}\' \
+	\'{"enabled" = 0;"name" = "IMAGES";}\' \
+	\'{"enabled" = 0;"name" = "BOOKMARKS";}\' \
+	\'{"enabled" = 0;"name" = "MUSIC";}\' \
+	\'{"enabled" = 0;"name" = "MOVIES";}\' \
+	\'{"enabled" = 0;"name" = "FONTS";}\' \
+	\'{"enabled" = 0;"name" = "MENU_OTHER";}\' \
+	\'{"enabled" = 0;"name" = "SOURCE";}\' \
+
+',
+//	\'{"enabled" = 0;"name" = "MENU_WEBSEARCH";}\' \
+        ],
+    ],
 ];
 
 
@@ -1062,29 +1179,6 @@ sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Vol
 # 	MENU_SPOTLIGHT_SUGGESTIONS (send search queries to Apple)
 # 	MENU_WEBSEARCH             (send search queries to Apple)
 # 	MENU_OTHER
-defaults write com.apple.spotlight orderedItems -array \
-	'{"enabled" = 1;"name" = "APPLICATIONS";}' \
-	'{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
-	'{"enabled" = 1;"name" = "DIRECTORIES";}' \
-	'{"enabled" = 1;"name" = "PDF";}' \
-	'{"enabled" = 1;"name" = "FONTS";}' \
-	'{"enabled" = 0;"name" = "DOCUMENTS";}' \
-	'{"enabled" = 0;"name" = "MESSAGES";}' \
-	'{"enabled" = 0;"name" = "CONTACT";}' \
-	'{"enabled" = 0;"name" = "EVENT_TODO";}' \
-	'{"enabled" = 0;"name" = "IMAGES";}' \
-	'{"enabled" = 0;"name" = "BOOKMARKS";}' \
-	'{"enabled" = 0;"name" = "MUSIC";}' \
-	'{"enabled" = 0;"name" = "MOVIES";}' \
-	'{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-	'{"enabled" = 0;"name" = "SPREADSHEETS";}' \
-	'{"enabled" = 0;"name" = "SOURCE";}' \
-	'{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
-	'{"enabled" = 0;"name" = "MENU_OTHER";}' \
-	'{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
-	'{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
-	'{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
-	'{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
 # Load new settings before rebuilding the index
 killall mds > /dev/null 2>&1
 # Make sure indexing is enabled for the main volume
