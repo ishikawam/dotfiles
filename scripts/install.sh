3118f6fe9da4209186f593e7694881926216d4f6
M_Ishikawa <ishikawam@nifty.com>
2019-01-18 15:40:09 +0900

    /Library/をsubmodule化

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,11 +10,21 @@ git clone -n git@github.com:ishikawam/dotfiles.git
 mv dotfiles/.git ./
 rm -r dotfiles
 git reset
+
 # 既存ファイルと衝突した分(modified)を~/tmp/に退避する
 mkdir ~/tmp/ ; git status | grep modified: | grep -o "[^ ]*$" | xargs -n 1 -I{} cp {} ~/tmp/
+
 git checkout .
 chmod 600 ~/.ssh/config
+
+# submodule
+if [ `uname` = "Darwin" ]; then
+    echo > .git/info/exclude
+else
+    echo /Library/ > .git/info/exclude
+fi
 git submodule update --init
+
 # 既存ファイルと衝突した分(add)を~/tmp/に退避する
 git status -sb | grep "^??" | grep -o "[^ ]*$" | xargs -n 1 -I{} mv {} ~/tmp/
 
