bbc91dba279f4fbcdc8348875b779ae405c9c351
M_Ishikawa <ishikawam@nifty.com>
2020-09-13 19:45:22 +0900

    emacs27

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,8 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+(when (< emacs-major-version 27)
+  (package-initialize))
 
 (setq preferences-directory "~/.emacs.d/")
 
@@ -17,6 +18,8 @@
 
 (cond
 ;; 26はcaskが動かないのでstraight.el
+ ((string-match "^27\." emacs-version)
+  (load-file-in-dir preferences-directory "init26.el"))
  ((string-match "^26\." emacs-version)
   (load-file-in-dir preferences-directory "init26.el"))
  ((string-match "^25\." emacs-version)
