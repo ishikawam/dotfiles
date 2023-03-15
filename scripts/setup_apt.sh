bd4552856d06dfc65ea8b3625f89608119e74da3
M_Ishikawa <ishikawam@nifty.com>
2023-03-15 16:50:44 +0900

    LC_ALL = C.UTF-8. apt setup.

---
+++ scripts/setup_apt.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+if ! type 'apt' >/dev/null 2>&1 ;then
+    # aptではない
+    exit
+fi
+
+echo
+read -n 1 -p "sudo apt install ... ok? (y/n) : " -a yn
+echo
+
+if [[ "$yn" != [yY] ]]; then
+    exit
+fi
+
+sudo apt install -y make emacs zsh php php-mbstring tig
+
+# 入れないもの
+# language-pack-ja
+
+loginshell=`grep $USER /etc/passwd | cut -d: -f7 | sed -e 's/^.*\///'`
+if [ ! "$loginshell" = 'zsh' ]; then
+    echo "shellをzshに変更"
+
+    if [ -f /bin/zsh ]; then
+        chsh -s /bin/zsh
+    elif [ -f /usr/bin/zsh ]; then
+        chsh -s /usr/bin/zsh
+    elif [ -f /usr/local/bin/zsh ]; then
+        chsh -s /usr/local/bin/zsh
+    else
+        chsh -s `which zsh`
+    fi
+
+    echo "$loginshell -> `which zsh`"
+fi
