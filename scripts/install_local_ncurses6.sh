92d20d4dafc586e876ef0c723419a26f53c74f06
M_Ishikawa <ishikawam@nifty.com>
2023-10-08 16:42:24 +0900

    ncurses 6.2 -> 6.4

--- scripts/install_local_ncurses6.sh
+++ scripts/install_local_ncurses6.sh
@@ -5,7 +5,7 @@
 
 # http://ftp.gnu.org/pub/gnu/ncurses で最新バージョンを確認、更新。
 
-version=6.2
+version=6.4
 
 echo "Check latest version."
 echo "> http://ftp.gnu.org/pub/gnu/ncurses"
@@ -28,6 +28,15 @@ tar zxvf ncurses-$version.tar.gz
 cd ncurses-$version
 mkdir -p ~/opt
 # CFLAGS='-O -fPIC' しないとzshでエラー
-CFLAGS='-O -fPIC' ./configure --prefix=$HOME/opt --enable-cflags="-I$HOME/opt/include" --enable-cppflags="-I$HOME/opt/include" --enable-ldflags="-L$HOME/opt/lib" --enable-multibyte --enable-locale --with-tcsetpgrp
+CFLAGS='-O -fPIC' ./configure \
+      --prefix=$HOME/opt \
+      --enable-cflags="-I$HOME/opt/include" \
+      --enable-cppflags="-I$HOME/opt/include" \
+      --enable-ldflags="-L$HOME/opt/lib" \
+      --enable-multibyte \
+      --enable-locale \
+      --with-tcsetpgrp
 make
 make install
+
+cd -
