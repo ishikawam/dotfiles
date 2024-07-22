aea6dfd01cc27e4064577c2e6dd7ab588837336a
M_Ishikawa <ishikawam@nifty.com>
2024-07-23 07:07:01 +0900

    PATH rancher desktop (/.rd/bin)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,3 @@
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-set --export --prepend PATH "/Users/masayuki.ishikawa/.rd/bin"
+set --export --prepend PATH "~/.rd/bin"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
